b244ec339cf8ce86717b7b8a2133ab16c48e6007
Plana1652 <ArisOfficial123@outlook.com>
2025-10-31 20:52:40 +0800

    Add files via upload

---
+++ Plana‘s CustomAchievementsLoad 251011+/Scripts/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using Nebula;
+using UnityEngine;
+public class PDebug
+{
+    public static void Log(object message)
+    {
+        if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/LOG"))
+        {
+            File.AppendAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/LOG/log.txt", message.ToString() + Environment.NewLine);
+        }
+        File.AppendAllText(Application.dataPath + "/MoreRolesLog.txt", message.ToString()+Environment.NewLine);
+    }
+}
